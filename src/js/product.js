f115c07f23f3fc9652aba45a0cda80d4506b266d
vikyrg05 <vikyrg05@gmail.com>
2026-07-09 17:09:24 -0400

    Refactor product details into ProductDetails class

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,10 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
-import ProductData from './ProductData.mjs';
+import { getParam } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData('tents');
+const productId = getParam("product");
+const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  let cart = getLocalStorage('so-cart') || [];
-  cart.push(product);
-  setLocalStorage('so-cart', cart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
