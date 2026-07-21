3976ffc777ef7b83bfcb30c6c67916d73e9600ee
vikyrg05 <vikyrg05@gmail.com>
2026-07-21 12:08:47 -0400

    Refactor ProductData to ExternalServices and add checkout POST

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -10,7 +10,7 @@ function convertToJson(res) {
   }
 }
 
-export default class ProductData {
+export default class ExternalServices {
   constructor() {
 
   }
@@ -25,5 +25,18 @@ export default class ProductData {
     const response = await fetch(`${baseURL}product/${id}`);
     const data = await convertToJson(response);
     return data.Result;
+  }
+
+  async checkout(order) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(order),
+    };
+
+    const response = await fetch(`${baseURL}checkout`, options);
+    return await convertToJson(response);
   }
 }
